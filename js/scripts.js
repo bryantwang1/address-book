633e02f86f19874c0a572d0e786f5d4febdb534b
Bryant Wang and Frank Gavin <frank@>
2016-11-01 08:17:25 -0700

    add an addresses array and an Address constructor to our contact book.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,11 @@ function Contact(first, last) {
   this.lastName = last;
   this.addresses = [];
 }
-
+function Address(street, city, state) {
+    this.street = street;
+    this.city = city;
+    this.state = state;
+}
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
